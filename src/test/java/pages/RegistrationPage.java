7e0b169e1cf2d7ed26c1fd357e726771968cb33b
allexmorozov <ouksss33@gmail.com>
2022-02-12 15:40:57 +0300

    add more changes

---
+++ src/test/java/pages/RegistrationPage.java
@@ -0,0 +1,77 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class RegistrationPage {
+    //locators
+    private SelenideElement
+
+            headerTitle = $(".main-header"),
+
+            firstNameInput = $("#firstName"),
+            lastNameInput = $("#lastName"),
+            userEmailInput = $("#userEmail"),
+            genderRadioButton = $(byText("Male")),
+            userNameInput = $("#userNumber"),
+
+            firstHobbiesCheckBox = $(byText("Sports")),
+            thirdHobbiesCheckBox = $(byText("Music")),
+            uploadPictureForm = $("#uploadPicture"),
+            currentAddressField = $("#currentAddress");
+
+
+
+    public RegistrationPage openPage() {
+        open("/automation-practice-form");
+        headerTitle.shouldHave(text("Practice Form"));
+        return this;
+    }
+
+    public RegistrationPage setFirstName(String firstName) {
+        firstNameInput.setValue(firstName);
+        return this;
+    }
+
+    public RegistrationPage setLastName(String lastName) {
+        lastNameInput.setValue(lastName);
+        return this;
+    }
+
+    public RegistrationPage setUserEmail(String userEmail) {
+        userEmailInput.setValue(userEmail);
+        return this;
+    }
+
+    public RegistrationPage clickGenderRadio() {
+        genderRadioButton.click();
+        return this;
+    }
+
+    public RegistrationPage setUserNumber(String userNumber) {
+        userNameInput.setValue(userNumber);
+        return this;
+    }
+
+    //
+    public void clickFirstHobbies() {
+        firstHobbiesCheckBox.click();
+    }
+
+    public void clickOneMoreHobbies() {
+        thirdHobbiesCheckBox.click();
+    }
+
+    public void uploadPicture(String uploadPicture) {
+        uploadPictureForm.uploadFromClasspath("raja.jpg");
+    }
+
+    public void setCurrentAddress(String currentAddress) {
+        currentAddressField.setValue(currentAddress);
+    }
+
+}
---
+++ src/test/java/tests/RegistrationFormTests.java
@@ -0,0 +1,71 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import pages.RegistrationPage;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class RegistrationFormTests {
+    RegistrationPage registrationPage = new RegistrationPage();
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void successFillTest() {
+
+        registrationPage.openPage()
+
+                        .setFirstName("Alex")
+                        .setLastName("Frost")
+                        .setUserEmail("alex@work.com")
+                        .clickGenderRadio()
+                        .setUserNumber("1234567890");
+
+        registrationPage.clickFirstHobbies();
+        registrationPage.clickOneMoreHobbies();
+        registrationPage.uploadPicture("raja.jpg");
+        registrationPage.setCurrentAddress("Lenina street 22");
+
+
+
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").selectOption("1991");
+        $(".react-datepicker__month-select").selectOption("May");
+        $(byText("8")).click();
+        $("#subjectsInput").setValue("civics");
+        $(byText("Civics")).click();
+//        $(byText("Sports")).click();
+//        $(byText("Music")).click();
+//        $("#uploadPicture").uploadFromClasspath("raja.jpg");
+//        $("#currentAddress").setValue("Lenina street 22");
+        $("#stateCity-label").scrollTo();
+        $(byText("Select State")).click();
+        $(byText("Haryana")).click();
+        $(byText("Select City")).click();
+        $(byText("Karnal")).click();
+        $("#submit").click();
+
+
+        $(".table-responsive").shouldHave(text("Alex"),
+                text("Frost"),
+                text("alex@work.com"),
+                text("Male"),
+                text("1234567890"),
+                text("8 May,1991"),
+                text("Civics"),
+                text("Sports, Music"),
+                text("Lenina street 22"),
+                text("Haryana Karnal"));
+
+
+    }
+}
+
