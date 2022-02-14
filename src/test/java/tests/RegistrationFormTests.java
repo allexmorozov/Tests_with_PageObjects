6956da352cd4c75a0ce525e4f6727fb6ea907d18
allexmorozov <ouksss33@gmail.com>
2022-02-14 12:51:18 +0300

    removed empty lines

--- src/test/java/tests/RegistrationFormTests.java
+++ src/test/java/tests/RegistrationFormTests.java
@@ -5,13 +5,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-
 public class RegistrationFormTests {
     RegistrationPage registrationPage = new RegistrationPage();
+
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
@@ -20,25 +16,22 @@ public class RegistrationFormTests {
 
     @Test
     void successFillTest() {
-
         registrationPage.openPage()
-
-                        .setFirstName("Alex")
-                        .setLastName("Frost")
-                        .setUserEmail("alex@work.com")
-                        .clickGenderRadio()
-                        .setUserNumber("1234567890")
-                        .setBirthDate("9","August", "1977")
-                        .setSubjectInput("hindi")
-                        .clickFirstHobbies()
-                        .clickOneMoreHobbies()
-                        .uploadPicture("raja.jpg")
-                        .setCurrentAddress("Lenina street 22")
-                        .selectStateDropdown()
-                        .selectCityDropdown()
-                        .clickSubmitButton()
-
-                        .checkForm();
+                .setFirstName("Alex")
+                .setLastName("Frost")
+                .setUserEmail("alex@work.com")
+                .clickGenderRadio()
+                .setUserNumber("1234567890")
+                .setBirthDate("9", "August", "1977")
+                .setSubjectInput("hindi")
+                .clickFirstHobbies()
+                .clickOneMoreHobbies()
+                .uploadPicture("raja.jpg")
+                .setCurrentAddress("Lenina street 22")
+                .selectStateDropdown()
+                .selectCityDropdown()
+                .clickSubmitButton()
+                .checkForm();
     }
 }
 
